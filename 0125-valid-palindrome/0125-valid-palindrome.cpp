94b9a34e539199d07ec62eceb827b900e2f18532
sneha-goyal7 <snehagoyal4295@gmail.com>
2026-09-22 19:35:29 +0530

    Time: 4 ms (13.83%), Space: 10 MB (52.2%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+bool isalphanumeric(char ch){
+    if((ch>='0' && ch<='9')||(tolower(ch)>='a' && tolower(ch)<='z')){
+        return true;
+    }
+    return false;
+}
+    bool isPalindrome(string s) {
+        int st=0,end=s.size()-1;
+        while(st<end){
+            if(!isalphanumeric(s[st])){
+                st++;
+                continue;
+            }
+            if(!isalphanumeric(s[end])){
+                end--;
+                continue;
+            }
+            if(tolower(s[st])!=tolower (s[end]) )return false;
+            st++;
+            end--;
+        }
+        return true;
+    }
+};
